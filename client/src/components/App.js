32724d0e4d6f56ab6239f9d06129884a2bb8e77f
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-30 21:29:24 +0100

    demonstration of React Router navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo" >Go to PageTwo</Link>
+    </div>
+  )
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me</button>
+      <Link to="/" >Go to PageOne</Link>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
     </div>
   )
 };
