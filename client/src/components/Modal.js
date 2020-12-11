cafebbe325ed958e775000573b7b6a7b8976c73d
NightmanCZ90 <novak.jan90@gmail.com>
2020-12-11 20:59:49 +0100

    added onDismiss method to further reuse the Modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
 
 
   return ReactDOM.createPortal(
     <div 
-      onClick={() => history.push('/')} 
+      onClick={props.onDismiss} 
       className="ui dimmer modals visible active"
     >
       <div 
